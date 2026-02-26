646cd37d0e4a045286d4df13c86985ed7b358461
ghanicodes <abdulghaniag1010@gmail.com>
2026-02-26 12:52:29 +0500

    vercel.json

--- Expense tracker/backend/server.js
+++ Expense tracker/backend/server.js
@@ -37,5 +37,10 @@ app.get('/', (req, res)=>{
     res.send("Hello Express")
 })
 
-app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () =>
+    console.log(`Server running on port ${PORT}`)
+  );
+}
 module.exports = app
